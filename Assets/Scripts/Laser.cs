c2771d3e3fe777aa3762a5b3daa0d5302aee31aa
Peter Jenkin <peternjenkin@gmail.com>
2020-01-08 20:04:38 +0000

    5-33 Laser behaviour - script added and written to translate prefab instatiated laser objects (moving upward)

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float _speed = 8f;      // 5-32 Laser behaviour (speed of 8)
+
+    void Start()
+    {
+        // the laser object is already instantiated by the Player object, at the Player's location   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);  // 5-32 Laser behaviour (movement upward)
+    }
+}
