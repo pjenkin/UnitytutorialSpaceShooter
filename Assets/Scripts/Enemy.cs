72988e288e697ac9ad5fa4ab0c69b691a9710297
Peter Jenkin <peternjenkin@gmail.com>
2020-01-10 15:19:01 +0000

    6-43 Enemy object behaviour script - random spawn, downward move

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 4.0f;
+    [SerializeField]
+    private float _enemy_bottom_max = -3.8f;
+    [SerializeField]
+    private float _enemy_top_max = 3.8f;
+    [SerializeField]
+    private float _enemy_left_side_max = -8.5f;
+    [SerializeField]
+    private float _enemy_right_side_max = 8.5f;
+    [SerializeField]
+    private float _enemy_random_x_pos = 0f;
+
+    // private Random _rand;       // System.Random not needed, and this would be Unity.Random (can use abstract)
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +28,18 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // 6-43 move enemy object downwards at 4 units/metres per second
-        // when off the bottom of the screen, respawn object at top at a random x position
+        // 6-43 move enemy object downwards at 4 units/metres per second (translate) - challenge
+        // when off the bottom of the screen, respawn object at top at a random x position (if then position)
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y <= _enemy_bottom_max)
+        {
+            // _rand = new Random();   // System.Random not needed, and this would be Unity.Random (can use abstract)
+            _enemy_random_x_pos =  Random.Range(_enemy_left_side_max, _enemy_right_side_max);
+            //Mathf.Clamp(_enemy_random_x_pos, _enemy_left_side_max, _enemy_right_side_max);
+
+            // transform.Translate(new Vector3(_enemy_random_x_pos, _enemy_top_max, 0));    
+            transform.position = new Vector3(_enemy_random_x_pos, _enemy_top_max, 0);       // NB Translate to move relative; position to set absolute! 6-43
+        }
     }
 }
