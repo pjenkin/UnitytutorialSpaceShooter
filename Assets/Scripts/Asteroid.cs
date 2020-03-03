dab54b47256c0028cc38a4c323ba22b3ae132c5e
Peter Jenkin <peternjenkin@gmail.com>
2020-03-03 20:21:23 +0000

    13-108 Asteroid behaviour-rotation added after asteroid sprite with collider & rigidbody added

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _speed = 3.0f;
+    [SerializeField]
+    private float _asteroid_bottom_max = -3.8f;
+    [SerializeField]
+    private float _asteroid_top_max = 3.8f;
+    [SerializeField]
+    private float _asteroid_left_side_max = -8.5f;
+    [SerializeField]
+    private float _asteroid_right_side_max = 8.5f;
+    [SerializeField]
+    private float _asteroid_random_x_pos = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime * 0);    // stay on the spot for the mo
+        Rotate();   // rotate the asteroid 13-108 Asteroid behaviour
+    }
+
+    /// <summary>
+    /// Rotate an asteroid sprite at a given angular velocity
+    /// 13-108 Asteroid behaviour
+    /// </summary>
+    /// <param name="angular_speed"></param>
+    public void Rotate(float angular_speed = 1f)
+    {
+        transform.Rotate(Vector3.forward * angular_speed);
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// (in 12-102 R key to restart level)
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private bool _isGameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void GameOver()
+    {
+        _isGameOver = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if the R key was pressed then restart the current scene
+        if (Input.GetKeyDown(KeyCode.R) && _isGameOver)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);     // 12-101 Key R to restart 'level' - scene named actually "Game" - File/Build Settings/Add Open Scenes - or index 0
+            // SceneManager.LoadScene(0);     // 12-101 Key R to restart 'level' - scene named actually "Game" - File/Build Settings/Add Open Scenes - or index 0
+            // SceneManager.LoadScene("Game");     // 12-101 Key R to restart 'level' - scene named actually "Game" - File/Build Settings/Add Open Scenes - or index 0
+        }
+
+        // text to appear when game over only (lives at zero)
+        // pressable only when game over (lives at zero)
+        // reload the Unity Scene    
+
+    }
+}
